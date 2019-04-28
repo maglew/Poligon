83e70a494e63afe6166705fceab36110a8b8daf6
maglew <v.crav4uk@yandex.ru>
2019-04-28 12:14:15 +0300

    first

---
+++ src/pack/states/MenuState.java
@@ -0,0 +1,23 @@
+package pack.states;
+
+import java.awt.Graphics;
+
+import pack.Game;
+
+public class MenuState extends State {
+
+	public MenuState(Game game){
+		super(game);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+	
+}
---
+++ src/pack/states/State.java
@@ -0,0 +1,32 @@
+package pack.states;
+
+import java.awt.Graphics;
+
+import pack.Game;
+
+
+public abstract class State {
+
+	private static State currentState = null;
+	
+	public static void setState(State state){
+		currentState = state;
+	}
+	
+	public static State getState(){
+		return currentState;
+	}
+	
+	//CLASS
+	
+	protected Game game;
+	
+	public State(Game game){
+		this.game = game;
+	}
+	
+	public abstract void tick();
+	
+	public abstract void render(Graphics g);
+	
+}
