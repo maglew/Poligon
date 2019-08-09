82d1b541f0388fbc8148c9373566a8a5064f8fa1
maglew <v.crav4uk@yandex.ru>
2019-08-09 17:03:38 +0300

    23

--- src/pack/entities/creatures/Creature.java
+++ src/pack/entities/creatures/Creature.java
@@ -37,12 +37,23 @@ public abstract class Creature extends Entity {
 					!collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)){
 				x += xMove;
 			}
-		}else if(xMove < 0){//Moving left
+			else
+			{
+				x=tx*Tile.TILEWIDTH-bounds.x-bounds.width-1;
+
+			}
+		}
+		else if(xMove < 0){//Moving left
 			int tx = (int) (x + xMove + bounds.x) / Tile.TILEWIDTH;
 
 			if(!collisionWithTile(tx, (int) (y + bounds.y) / Tile.TILEHEIGHT) &&
 					!collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)){
 				x += xMove;
+			}
+			else
+			{
+				x=tx*Tile.TILEWIDTH+Tile.TILEWIDTH-bounds.x;
+
 			}
 		}
 	}
@@ -55,12 +66,23 @@ public abstract class Creature extends Entity {
 					!collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)){
 				y += yMove;
 			}
+			else
+			{
+				y=ty*Tile.TILEHEIGHT+Tile.TILEHEIGHT-bounds.y;
+
+
+			}
 		}else if(yMove > 0){//Down
 			int ty = (int) (y + yMove + bounds.y + bounds.height) / Tile.TILEHEIGHT;
 
 			if(!collisionWithTile((int) (x + bounds.x) / Tile.TILEWIDTH, ty) &&
 					!collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)){
 				y += yMove;
+			}
+			else
+			{
+				y=ty*Tile.TILEHEIGHT-bounds.y-bounds.height-1;
+
 			}
 		}
 	}
